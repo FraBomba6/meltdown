b920941072dfb00cdfffe41ab8b5dbfc06da378c
bomb697 <50354770+bomb697@users.noreply.github.com>
2021-12-15 00:13:30 +0100

    Modified memory address

--- NaiveKernelAccess.c
+++ NaiveKernelAccess.c
@@ -1,4 +1,10 @@
-//
-// Created by Francesco on 15/12/2021.
-//
+#include <stdio.h>
+
+int main()
+{
+    char *kernel_data_addr = (char*)0xf9c91000;
+    char kernel_data = *kernel_data_addr;
+    printf("I have reached here.\n");
+    return 0;
+}
 
