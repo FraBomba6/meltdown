1e426bdc11fc350122bf8a6df5c361ae87801947
bomb697 <50354770+bomb697@users.noreply.github.com>
2021-12-14 19:55:54 +0100

    New CacheTime version

--- CacheTime.c
+++ CacheTime.c
@@ -4,30 +4,44 @@
 #include <emmintrin.h>
 #include <x86intrin.h>
 
-uint8_t array[10*4096];
+int times[10][10]
+
+void getCacheTimes(int cycle) {
+    uint8_t array[10*4096];
+    int junk=0;
+    register uint64_t time1, time2;
+    volatile uint8_t *addr;
+    int i;
+
+    // Initialize the array
+    for(i=0; i<10; i++) array[i*4096]=1;
+
+    // FLUSH the array from the CPU cache
+    for(i=0; i<10; i++) _mm_clflush(&array[i*4096]);
+
+    // Access some of the array items
+    array[3*4096] = 100;
+    array[7*4096] = 200;
+
+    for(i=0; i<10; i++) {
+        addr = &array[i*4096];
+        time1 = __rdtscp(&junk);
+        junk = *addr;
+        time2 = __rdtscp(&junk) - time1;
+        printf("Access time for array[%d*4096]: %d CPU cycles\n",i, (int)time2);
+        times[cycle][i] = (int)time2
+    }
+}
 
 int main(int argc, const char **argv) {
-  int junk=0;
-  register uint64_t time1, time2;
-  volatile uint8_t *addr;
-  int i;
-  
-  // Initialize the array
-  for(i=0; i<10; i++) array[i*4096]=1;
+    for(int i=0; i<10; i++)
+        getCacheTimes(i)
 
-  // FLUSH the array from the CPU cache
-  for(i=0; i<10; i++) _mm_clflush(&array[i*4096]);
-
-  // Access some of the array items
-  array[3*4096] = 100;
-  array[7*4096] = 200;
-
-  for(i=0; i<10; i++) {
-    addr = &array[i*4096];
-    time1 = __rdtscp(&junk);                
-    junk = *addr;
-    time2 = __rdtscp(&junk) - time1;       
-    printf("Access time for array[%d*4096]: %d CPU cycles\n",i, (int)time2);
-  }
-  return 0;
+    for(int i=0; i<10; i++){
+        sum = 0;
+        for(int j=0; j<10; j++){
+            sum += times[j][i]
+        }
+        printf("Mean access time for array[%d*4096] after 10 repetitions: %d CPU cycles\n",i, (int)sum/10))
+    }
 }
